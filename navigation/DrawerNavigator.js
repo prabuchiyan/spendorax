c03cf987c9d22575710b07dabf509de087db374c
prabuchiyan <prabuchiyan@gmail.com>
2026-07-07 20:58:56 +0530

    Version Update & New APK Created

--- navigation/DrawerNavigator.js
+++ navigation/DrawerNavigator.js
@@ -71,7 +71,7 @@ function CustomDrawerContent(props) {
         </View>
         <View style={styles.headerTextContainer}>
           <Text style={styles.appName}>SpendoraX</Text>
-          <Text style={styles.appVersion}>v2.3</Text>
+          <Text style={styles.appVersion}>v2.3.2</Text>
         </View>
       </View>
 
